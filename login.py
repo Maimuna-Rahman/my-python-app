cb4e5fcc0565189f2b18c93e959ef51432bd852d
Maimuna Rahman <mr2910808gmail.com>
2025-08-08 12:14:50 +0200

    passchange

--- login.py
+++ login.py
@@ -1,6 +1,7 @@
 # ❗ WARNING: This is a simulation. Never store passwords like this in real projects!
 USERNAME = "admin"
 PASSWORD = "P@ssw0rd123"
+DB_PASSWORD = "SuperSecret123"
 def authenticate(input_user, input_pass):
 if input_user == USERNAME and input_pass == PASSWORD:
 return "Login successful!"
